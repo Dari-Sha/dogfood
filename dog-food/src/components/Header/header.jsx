f462f161fc94c37911f3f04c6d1bf4fb4cfa92ab
Dari-Sha <utkina.dar@gmail.com>
2023-01-28 00:53:27 +0300

    login logout

--- dog-food/src/components/Header/header.jsx
+++ dog-food/src/components/Header/header.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import "./header.css";
 
 export default () => {
+   let user = localStorage.getItem("user8");
+   const logIn = (e) => {
+      e.preventDefault();
+      let name = prompt("Как Вас зовут?");
+      if (name) {
+         localStorage.setItem("user8", name);
+         user = name;
+      }
+   }
+   const logOut = (e) => {
+      e.preventDefault();
+      localStorage.removeItem("user8");
+   }
+   
    return <header>
       <a className="logo" href="">DogFood</a>
       <input type="search" placeholder="Поиск..." className="search"/>
       <nav className="menu">
-         <a href="">Личный кабинет</a>
-         <a href="">Войти</a>
-         <a href="">Выйти</a>
+         {user && <a href="">{user}</a>}
+         {!user && <a href="" onClick={logIn}>Войти</a>}
+         {<a href="" onClick={logOut}>Выйти</a>}
       </nav>
    </header> 
 }
