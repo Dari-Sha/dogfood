bc97f0737fed64d6cd5f8cd8f9091d682f1b7344
Dari-Sha <utkina.dar@gmail.com>
2023-01-28 17:27:06 +0300

    modals

--- dog-food/src/App.jsx
+++ dog-food/src/App.jsx
@@ -5,24 +5,34 @@ import products from "./assets/data.json";
 
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
-import Search from "./components/Search/search";
+//import Search from "./components/Search/search";
 import Catalog from "./pages/Catalog.jsx";
 import Home from "./pages/Home.jsx";
+import Modal from "./components/Modal";
 
 
 const smiles = ["=^_^=", "*_*", "O_o", "o_o", "o_O", "-_-"];
 
 const App = () => {
     const [user, setUser] = useState(localStorage.getItem("user8"));
+    const [modalActive, setModalActive] = useState(true);
     return (
+        <>
         <div className="container">
-        <Header user={user} setUser={setUser}/>
+        <Header 
+            user={user} 
+            setUser={setUser} 
+            products={products} 
+            setModalActive={setModalActive}
+        />
         <main>
-            <Search data={products}/>
             {user ? <Catalog data={products}/> : <Home data={smiles}/>};
         </main>
         <Footer/>
     </div>
+    {/* is active это пропсы, которые работают внутри компонента Модал. а модалэктив и сетмодалэктив - значения, которые сохраняются внутри параметра */}
+    <Modal isActive={modalActive} setState={setModalActive}/>
+    </>
     )
 }
 
--- dog-food/src/components/Header/header.jsx
+++ dog-food/src/components/Header/header.jsx
@@ -1,18 +1,24 @@
 import React, {useState} from "react";
+import Search from "../Search/search";
 import "./header.css";
 
-export default ({user, setUser}) => {
+export default ({user, setUser, products, setModalActive}) => {
    // хук состояния - свойство, функция, в качестве аргумента которой передается новое значение нашего свойства = useState(аргумент-изначальное значение свойства)
    // const [user, setUser] = useState(localStorage.getItem("user8"));
 
    // let user = localStorage.getItem("user8");
    const logIn = (e) => {
       e.preventDefault();
-      let name = prompt("Как Вас зовут?");
-      if (name) {
-         localStorage.setItem("user8", name);
-         setUser(name);
-      }
+      // let name = prompt("Как Вас зовут?");
+      // if (name) {
+      //    localStorage.setItem("user8", name);
+      //    setUser(name);
+      // }
+      setModalActive(prev => !prev);
+      // setModalActive(function(previous) {
+      //    console.log("Активность модального окна:", previous);
+      // return !previous;
+      // });
    }
    const logOut = (e) => {
       e.preventDefault();
@@ -22,7 +28,8 @@ export default ({user, setUser}) => {
    
    return <header>
       <a className="logo" href="">DogFood</a>
-      <input type="search" placeholder="Поиск..." className="search"/>
+      <Search data={products}/>
+      {/* <input type="search" placeholder="Поиск..." className="search"/> */}
       <nav className="menu">
          {user && <a href="">{user}</a>}
          {!user && <a href="" onClick={logIn}>Войти</a>}
